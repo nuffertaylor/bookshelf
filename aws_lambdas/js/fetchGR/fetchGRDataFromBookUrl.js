5d0cf62e302593e5af67fb3870b25ea368f7ff17
Taylor Nuffer <nuffertaylor@gmail.com>
2022-10-04 20:39:01 -0600

    remove querystr and numbered  title in gr fetch

--- aws_lambdas/js/fetchGR/fetchGRDataFromBookUrl.js
+++ aws_lambdas/js/fetchGR/fetchGRDataFromBookUrl.js
@@ -25,7 +25,12 @@ function get_book_data_from_book_page(page, book_id){
   const book_title = page.querySelector("#bookTitle").childNodes[0]._rawText.trim();
   let book_series = page.querySelector("#bookSeries");
   // check if book is in series
-  if(book_series.querySelector("a")) book_series = " " + book_series.querySelector("a").childNodes[0]._rawText.trim();
+  if(book_series.querySelector("a")) {
+    book_series = " " + book_series.querySelector("a").childNodes[0]._rawText.trim();
+    //add a comma between the number and the series number
+    const temp = book_series.split('#');
+    book_series = temp[0].trim() + ", #" + temp[1];
+  }
   else book_series = "";
   const gr_title = book_title + book_series;
   const author_name = page.querySelector(".authorName").querySelector('[itemprop="name"]').childNodes[0]._rawText.trim();
@@ -41,16 +46,20 @@ function get_book_data_from_book_page(page, book_id){
     num_pages : num_pages,
     genre : genre
   };
-}
+};
+const remove_query_string = (url) => { return url.split('?')[0]; };
+const remove_text_title = (url) => { return url.split('-')[0]; };
 
 const main = async function (url){
   if(!url) return {};
+  url = remove_query_string(url);
+  url = remove_text_title(url);
   const book_id = remove_non_numeric_char_from_str(url);
   const book_url = get_book_url_from_book_id(book_id);
   const page_HTML = await fetch_page(book_url);
   const page = parse_html(page_HTML);
   const book = get_book_data_from_book_page(page, book_id);
   return book;
-}
+};
 
 export default main;
