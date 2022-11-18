655c7db9aa28cd6b7b53db58c09b2c9c1bba8659
Taylor Nuffer <nuffertaylor@gmail.com>
2022-11-17 19:29:40 -0700

    changed results to be based on query type

--- aws_lambdas/python/getShelfBgsLambda.py
+++ aws_lambdas/python/getShelfBgsLambda.py
@@ -9,12 +9,13 @@ def lambda_handler(event, context):
   results = None
   if("bg_id" in event.keys() and event["bg_id"]):
     results = db.get_shelf_bg_by_bg_id(event["bg_id"])
+    if(not results): results = {}
   elif("filename" in event.keys() and event["filename"]):
     results = db.get_shelf_bg_by_filename(event["filename"])
+    if(not results): results = {}
   else:
     results = db.get_all_shelf_bgs()
-  if(results):
-    return(200, results)
-  return(200, [])
+    if(not results): results = []
+  return(200, results)
 
 # aws lambda update-function-code --function-name getShelfBgs --zip-file fileb://~/projects/bookshelf/aws_lambdas/python/lambda.zip
