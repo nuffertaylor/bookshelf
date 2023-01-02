46e2874824ab124feda2e8682f708a6d1a20df53
Taylor Nuffer <nuffertaylor@gmail.com>
2023-01-01 23:39:12 -0700

    added limit to max shelf name length

--- aws_lambdas/python/setShelfOwner.py
+++ aws_lambdas/python/setShelfOwner.py
@@ -10,6 +10,7 @@ def lambda_handler(event, context):
   if("filename" not in event.keys()): return httpResult(403, "Request failed, no filename provided")
   if("authtoken" not in event.keys()): return httpResult(403, "Request failed, no authtoken provided")
   bookshelf_name = event["bookshelf_name"] if event["bookshelf_name"] else ""
+  if(len(bookshelf_name) > 64): bookshelf_name = bookshelf_name[:63]
   if(not db.validate_username_authtoken(event["username"], event["authtoken"])):
     return httpResult(403, "Request Failed, invalid authtoken")
   if(db.set_shelf_image_owner(event["filename"], event["username"], bookshelf_name)):
