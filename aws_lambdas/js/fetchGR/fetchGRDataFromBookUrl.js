90c297e86ad6e69303dcc6989beb906e196aed63
Taylor Nuffer <nuffertaylor@gmail.com>
2023-02-12 23:51:05 -0700

    cleaned up fetchGR

--- aws_lambdas/js/fetchGR/fetchGRDataFromBookUrl.js
+++ aws_lambdas/js/fetchGR/fetchGRDataFromBookUrl.js
@@ -9,70 +9,13 @@ function get_book_url_from_book_id(book_id){
   return URL_PREFIX + book_id;
 }
 
-//@DEPRECATED
-//As of Jan 2023, The Goodreads site change has made this function useless.
-function get_book_data_from_book_page(page, book_id){
-  let book_title = "";
-  const book_title_el = page.querySelector("#bookTitle");
-  if(book_title_el && book_title_el.childNodes)
-    book_title = book_title_el.childNodes[0]._rawText.trim();
-  
-  let book_series = page.querySelector("#bookSeries");
-  // check if book is in series
-  if(book_series.querySelector("a")) {
-    book_series = " " + book_series.querySelector("a").childNodes[0]._rawText.trim();
-    //add a comma between the number and the series number
-    const temp = book_series.split('#');
-    if(temp.length > 1) book_series = temp[0].trim() + ", #" + temp[1];
-    else book_series = temp[0].trim();
-  }
-  else book_series = "";
-  const gr_title = book_title + " " + book_series;
-
-  //TODO: Handling for multiple authors (currently will only grab one)
-  let author_name = "";
-  const author_name_el = page.querySelector(".authorName");
-  if(author_name_el) {
-    const name_prop = author_name_el.querySelector('[itemprop="name"]');
-    if(name_prop && name_prop.childNodes) author_name = name_prop.childNodes[0]._rawText.trim();
-  }
-  
-  let num_pages = "";
-  const num_pages_el = page.querySelector('[itemprop="numberOfPages"]');
-  if(num_pages_el && num_pages_el.childNodes) {
-    num_pages = remove_non_numeric_char_from_str(page.querySelector('[itemprop="numberOfPages"]').childNodes[0]._rawText.trim());
-  }
-
-  //TODO: This successfully gets the publication date of the provided edition. But more useful to the user is the original publication date.
-  let pub_date_string = ""
-  const details_element = page.querySelector('#details');
-  if(details_element) {
-    const rows = details_element.querySelectorAll(".row");
-    if(rows && rows.length > 1)
-      if(rows[1] && rows[1].childNodes) 
-        pub_date_string = rows[1].childNodes[0]._rawText.trim().split("\n")[1].trim();
-  }
-  let genre = "";
-  const genre_el = page.querySelector(".bookPageGenreLink");
-  if(genre_el && genre_el.childNodes)
-    genre = genre_el.childNodes[0]._rawText.trim();
-  return {
-    book_id : book_id,
-    title : gr_title,
-    author : author_name,
-    pubDate : pub_date_string,
-    num_pages : num_pages,
-    genre : genre
-  };
-}
-
 const find_json_in_str = (str) => {
   let found_json = [];
   let brace_stack = [];
   for(let i = 0; i < str.length; i++){
     if(str.charAt(i) === '{' && str.charAt(i+1) === '"') {
       //push the position of the brace
-      brace_stack.push(i)
+      brace_stack.push(i);
     }
     else if(str.charAt(i) === '}') {
       if(brace_stack.length > 0){
@@ -86,15 +29,7 @@ const find_json_in_str = (str) => {
     }
   }
   return found_json;
-}
-
-const parse_list_of_json = (json_list) => {
-  let parsed_json = []
-  for(const j of json_list){
-    parsed_json.push(JSON.parse(j));
-  }
-  return parsed_json;
-}
+};
 
 const get_last_sub_dir_from_url = (url) => {
   let res = url.split('/').at(-1);
@@ -125,12 +60,12 @@ const main = async function (url){
     statusCode: page.statusCode,
     body: {
       book_id : book_id,
-      title : parsed_json.name,
+      title : parsed_json.name.replace(/&apos;/g, "'"),
       isbn: parsed_json.isbn,
       author : parsed_json.author[0].name,
       num_pages : parsed_json.numberOfPages,
     }
-  }
+  };
 };
 
 export default main;
